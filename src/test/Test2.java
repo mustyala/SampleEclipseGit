4801b6a7001e8d9979c862c1e5e77be2832b7220
mustyala <veena.mustyala@gmail.com>
2016-07-20 20:06:02 +1000

    partial implementaion of printing numbers

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -1,9 +1,12 @@
 package test;
 
 public class Test2 {
-
+ int data[]={1,4,67};
+ 
 	public void test2(){
-		System.out.println(" Executing test2");
+		for(int i=0; i<data.length; i++){
+			System.out.println("");
+		}
 	}
 
 }
