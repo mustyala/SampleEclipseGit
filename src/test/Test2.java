a05fe4a9691e99a72940207c7b58c4c80d173a66
mustyala <veena.mustyala@gmail.com>
2016-07-20 20:16:45 +1000

    updated print number in test2 object

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -4,8 +4,9 @@ public class Test2 {
  int data[]={1,4,67};
  
 	public void test2(){
+		System.out.println("Data:");
 		for(int i=0; i<data.length; i++){
-			System.out.println("");
+			System.out.println("next value" + data[i]);
 		}
 	}
 
