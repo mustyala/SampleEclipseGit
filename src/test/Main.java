10c628b1354898442e03469c7e5c2ab3b2b6b6cc
mustyala <veena.mustyala@gmail.com>
2016-07-20 14:05:59 +1000

    First check in

---
+++ src/test/Main.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Test2 t = new Test2();
+		
+		test1();
+		t.test2();
+		
+	}
+	
+	public static void test1(){
+		System.out.println("Execute test1 ");
+	}
+
+}
---
+++ src/test/Test2.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test2 {
+
+	public void test2(){
+		System.out.println(" Executing test2");
+	}
+
+}
