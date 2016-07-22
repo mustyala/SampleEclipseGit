d11ed29096c3742a419e0f9c0ee11c009b144fb2
mustyala <veena.mustyala@gmail.com>
2016-07-22 13:47:28 +1000

    Summed up values displaying

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -1,13 +1,21 @@
 package test;
+/**
+ *
+ * @author veena
+ *
+ */
 
 public class Test2 {
  int data[]={1,4,67};
  
 	public void test2(){
 		System.out.println("Data:");
+		int sum=0;
 		for(int i=0; i<data.length; i++){
 			System.out.println("next value" + data[i]);
+			 sum += i;
 		}
+		System.out.println("Sum is : " + sum);
 	}
 
 }
