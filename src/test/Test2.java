734200b7e9e561845cda51881440b658658937ae
mustyala <veena.mustyala@gmail.com>
2016-07-22 13:42:30 +1000

    updated array default values

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -1,11 +1,12 @@
 package test;
 
 public class Test2 {
- int data[]={1,4,67};
+ int data[]={-3, 1,4,67, 44};
  
 	public void test2(){
 		System.out.println("Data:");
-		for(int i=0; i<data.length; i++){
+		for(int i: data){
+			
 			System.out.println("next value" + data[i]);
 		}
 	}
